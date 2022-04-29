256dbefd0a4f345a7c18754839a6f1c9ef563858
Anis <anis@laib.me>
2022-04-29 01:33:50 +0100

    added image + responsive UI

--- faq.js
+++ faq.js
@@ -2,18 +2,18 @@ const accordionItems = document.querySelectorAll('.faq .items .container')
 
 accordionItems.forEach(item => {
     item.querySelector('.item .icon-img').addEventListener('click', event => {
-        // const activeItem = document.querySelector('.faq .items .active')
-        // if (activeItem && activeItem !== item.querySelector('.item-content')) {
-        //     activeItem.classList.remove('active')
-        //     activeItem.style.maxHeight = '0'
-        // }
+        const activeItem = document.querySelector('.faq .items .active')
+        if (activeItem && activeItem !== item.querySelector('.item-content')) {
+            activeItem.classList.remove('active')
+            activeItem.style.maxHeight = '0'
+        }
 
         if (!item.querySelector('.item-content').classList.contains('active')) {
             item.querySelector('.item-content').classList.add('active')
         }
 
         if (item.querySelector('.item-content').classList.contains('active')) {
-            item.querySelector('.item-content').style.maxHeight = item.querySelector('.item-content').scrollHeight + "px"
+            item.querySelector('.item-content').style.maxHeight = (item.querySelector('.item-content').scrollHeight - 1) + "px"
         } else {
             item.querySelector('.item-content').style.maxHeight = "0"
         }
